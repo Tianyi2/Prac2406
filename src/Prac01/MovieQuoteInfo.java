441103a1239b75d8401811d38ecae6d1a467d628
Tianyi2 <tianyi2332@163.com>
2022-11-15 16:09:09 +0800

    The second exercises (7: MovieQuoteInfo)

---
+++ src/Prac01/MovieQuoteInfo.java
@@ -0,0 +1,9 @@
+package Prac01;
+
+public class MovieQuoteInfo {
+    public static void main(String[] args){
+        System.out.println("This is the movie quote");
+        System.out.println("This is the character who said it");
+        System.out.println("This is the year of the movie");
+    }
+}
