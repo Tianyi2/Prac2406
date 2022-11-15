50d9d748af189e554ab41a9e53850816657bab91
Tianyi2 <tianyi2332@163.com>
2022-11-15 16:19:26 +0800

    The forth exercises (10: Comments)

---
+++ src/Prac01/Comments.java
@@ -0,0 +1,15 @@
+package Prac01;
+
+class Comments
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Program comments are nonexecuting statements you add to a file for the purpose of documentation.");
+        // line comments
+        // Program comments are nonexecuting statements you add to a file for the purpose of documentation.
+        // block comment
+        /* Program comments are nonexecuting statements you add to a file for the purpose of documentation. */
+        // javadoc comment
+        /** Program comments are nonexecuting statements you add to a file for the purpose of documentation.  */
+    }
+}
