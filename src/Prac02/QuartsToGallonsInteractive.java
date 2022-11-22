5d56dbf461e998909b795580a87a3154397edbd1
Tianyi2 <tianyi2332@163.com>
2022-11-22 15:39:01 +0800

    Programming Exercises 4b: QuartsToGallonsInteractive

--- src/Prac02/QuartsToGallonsInteractive.java
+++ src/Prac02/QuartsToGallonsInteractive.java
@@ -1,15 +1,22 @@
 package Prac02;
 
-class QuartsToGallons
+import java.util.Scanner;
+class QuartsToGallonsInteractive
 {    
    public static void main(String[] args) 
-   {  
+   {
       final int QUARTS_IN_A_GALLON = 4;
-      int numberQuartsNeeded = 18;
+      int numberQuartsNeeded;
       int gallonsNeeded;
       int quartsNeeded;
+
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter quarts needed >> ");
+      numberQuartsNeeded = scanner.nextInt();
+
       gallonsNeeded = numberQuartsNeeded / QUARTS_IN_A_GALLON;
       quartsNeeded = numberQuartsNeeded % QUARTS_IN_A_GALLON;
+
       System.out.printf("A job that needs %d quarts requires %d gallons plus %d quarts", numberQuartsNeeded, gallonsNeeded, quartsNeeded);
    }
 }
