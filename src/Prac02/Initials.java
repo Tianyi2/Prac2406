a2348a78f61a3eb02299968cadb0299e802dee3e
Tianyi2 <tianyi2332@163.com>
2022-11-22 15:41:04 +0800

    Programming Exercises 7: Initials

---
+++ src/Prac02/Initials.java
@@ -0,0 +1,12 @@
+package Prac02;
+
+class Initials
+{    
+   public static void main(String[] args) 
+   {  
+      char initial1 = 'J';
+      char initial2 = 'M';
+      char initial3 = 'F';
+      System.out.printf("%c.%c.%c", initial1, initial2, initial3);
+   }
+}
