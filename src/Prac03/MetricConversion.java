620178ae3fffc8627b70a8a4a53eb8e3536625b5
Tianyi2 <tianyi2332@163.com>
2022-11-29 15:32:49 +0800

    Complete chapter-3 Programming Exercises Question 7

---
+++ src/Prac03/MetricConversion.java
@@ -0,0 +1,31 @@
+package Prac03;
+
+import java.util.Scanner;
+public class MetricConversion
+{
+  public static void main (String[] args)
+  {
+     int userInteger;
+     Scanner input = new Scanner(System.in);
+     System.out.print("What is the integer: ");
+     userInteger = input.nextInt();
+     convertCentimeters(userInteger);
+     convertLiters(userInteger);
+  }
+
+  public static void convertLiters(int gallons){
+      double liters;
+      final double gallonsToLitersRate = 3.7854;
+      liters = gallons * gallonsToLitersRate;
+      System.out.printf("%d gallons is %.2f liters as one gallons is %.4f liters\n", gallons, liters, gallonsToLitersRate);
+  }
+
+  public static void convertCentimeters(int inches) {
+     double centimeters;
+     final double inchesToCentimetersRate = 2.54;
+     centimeters = inches * inchesToCentimetersRate;
+     System.out.printf("%d inches is %.2f centimeters as one inches is %.2f centimeters\n", inches, centimeters, inchesToCentimetersRate);
+  }
+
+
+}
