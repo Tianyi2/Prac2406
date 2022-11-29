e4cc00c8453b00a7c5f8fdf991a0bdc6abb4637e
Tianyi2 <tianyi2332@163.com>
2022-11-29 16:02:59 +0800

    Complete Chapter-4 Programming Exercises Question 1

---
+++ src/Prac03/FormLetterWriter.java
@@ -0,0 +1,23 @@
+package Prac03;
+
+public class FormLetterWriter
+{
+   public static void main(String[] args)
+   {
+      displaySalutation("Zhang");
+      displayLetter();
+      displaySalutation("Tianyi", "Zhang");
+      displayLetter();
+   }
+
+   public static void displaySalutation(String lastName){
+      System.out.println("Dear Mr. or Ms. " + lastName);
+   }
+
+   public static void displaySalutation(String firstName, String lastName){
+      System.out.println("Dear " + firstName + " " + lastName);
+   }
+   public static void displayLetter(){
+      System.out.println("Thank you for your recent order.\n");
+   }
+}
