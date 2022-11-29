5b4474d462f6e60a5cac84d6634bd7ef83c9d8d3
Tianyi2 <tianyi2332@163.com>
2022-11-29 15:35:49 +0800

    Solve DebugFour1

---
+++ src/Prac03/DebugFour1.java
@@ -0,0 +1,18 @@
+package Prac03;
+
+// This program assigns values to two variables
+// and performs mathematical operations with them
+public class DebugFour1
+{
+    public static void main(String[] args)
+{
+    int x = 5;
+    int y = 8;
+    System.out.println("adding " + x + " and " + y + " equal to " + (x+y));
+
+    int z = 19;
+    System.out.println("subtracting "  + z + " and " + y + " equal to " + (z-y));
+    System.out.println("dividing " + z + " and " + x + " equal to " + (z/x));
+    System.out.println("multiplying " + x + " and " + z + " equal to " + (x*z));
+}
+}
