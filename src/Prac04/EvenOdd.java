d1fb72ad35cccf216a6a0b8d06d176604f56af9f
Tianyi2 <tianyi2332@163.com>
2022-12-06 15:53:49 +0800

    Add comments

--- src/Prac04/EvenOdd.java
+++ src/Prac04/EvenOdd.java
@@ -1,22 +1,24 @@
 package Prac04;
 
 import java.util.Scanner;
-class EvenOdd
-{
-   public static void main(String[] args) 
-   {  
-       Scanner input = new Scanner(System.in);
-       int number;
-       System.out.print("Enter the number: ");
-       number = input.nextInt();
-       if(isEven(number))
-          System.out.println(number + " is even");
-       else
-          System.out.println(number + " is odd");
-   }
-   public static boolean isEven(int number)
-   {
-      return number % 2 == 0;
-   }      
+
+class EvenOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        System.out.print("Enter the number: ");
+        number = input.nextInt();
+
+        // Display message
+        if (isEven(number))
+            System.out.println(number + " is even");
+        else
+            System.out.println(number + " is odd");
+    }
+
+    public static boolean isEven(int number) {
+        // Return true if the number is even.
+        return number % 2 == 0;
+    }
 }
 
--- src/Prac04/Factorials.java
+++ src/Prac04/Factorials.java
@@ -4,11 +4,13 @@ public class Factorials {
     public static void main(String[] args) {
         final int MAXIMUM = 10;
         int factorial;
+        // loop the 10 number from 10 to 1
         for (int i = MAXIMUM; i >= 1; i--) {
             factorial = i;
             for (int j = i-1; j > 0; j--)
+                // calculate for the factorial
                 factorial = factorial * j;
-            System.out.println("The factorial of " + i + " is " + factorial);
+            System.out.println("The factorial of " + i + " is " + factorial); //Display message
         }
     }
 }
