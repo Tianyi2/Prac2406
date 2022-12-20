d0e231be268dae8c44e04da96cef3f2ab3cc5c7f
Tianyi2 <tianyi2332@163.com>
2022-12-20 15:19:27 +0800

    Solve DebugMusical

---
+++ src/Prac06/DebugMusical.java
@@ -0,0 +1,22 @@
+package Prac06;
+
+public class DebugMusical extends DebugPlay {
+    protected String composer;
+    String title;
+
+    public DebugMusical(String title, String author, String composer) {
+        super(title, author);
+        this.composer = composer;
+    }
+
+    public void display() {
+        System.out.println("The performance is" + title +
+                " by " + author + "\nThe music for " + title +
+                " is by " + composer);
+    }
+
+    public static void main(String[] args) {
+        DebugMusical musical = new DebugMusical("Title", "Author", "Composer");
+        musical.display();
+    }
+}
